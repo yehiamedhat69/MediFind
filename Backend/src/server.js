faae2ef497cde4b3c00f3fbcd926ce25f11f5eec
Yehia Medhat <yehiamedhiat@gmail.com>
2026-09-05 22:48:11 +0300

    feat: add user management endpoints

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-
+const userRoutes = require("./routes/userRoutes");
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 connectDB();
